a0efbb8e2ce544a8790bf7404e28583031e10487
Pyra <gaast.dir@skiff.com>
2023-09-11 14:51:40 +0200

    Decl rust modules

--- crates/venx_compute/src/lib.rs
+++ crates/venx_compute/src/lib.rs
@@ -2,7 +2,9 @@ mod bindgroup;
 mod buffer_ext;
 mod builders;
 mod compute_server;
+mod module;
 mod pipeline;
+mod tests;
 
 pub use bindgroup::{BindGroupBuilder, BindGroupVenx};
 pub use builders::BufferBuilder;
