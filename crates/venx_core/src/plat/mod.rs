cba0201a25dcaffe1ee16cbdad8832a17c5f90df
Pyra <gaast.dir@skiff.com>
2023-09-11 18:04:47 +0200

    Markup Plats

---
+++ crates/venx_core/src/plat/mod.rs
@@ -0,0 +1,12 @@
+use crate::{chunks::storage::ChunksStorage, controller::Controller};
+
+pub struct Plat {
+    controller: Controller,
+    chunks: ChunksStorage,
+}
+
+impl Plat {
+    pub fn load() {}
+    pub fn save() {}
+    pub fn load_mca() {}
+}
