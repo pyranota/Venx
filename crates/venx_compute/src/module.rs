b7fc13d39cca348257b5d83fe9ced674561b1c54
Pyra <gaast.dir@skiff.com>
2023-09-11 14:51:06 +0200

    Remove dbg!

--- crates/venx_compute/src/module.rs
+++ crates/venx_compute/src/module.rs
@@ -9,13 +9,8 @@ use anyhow::Result;
 
 impl ComputeServer {
     pub fn new_module(&mut self, src: &str) -> Result<ShaderModule> {
-        dbg!("Before");
         let naga = self.make_naga(src, "")?;
-        dbg!("After make naga");
-
         let _ = self.load_composable(src, "");
-
-        dbg!("After");
         Ok(self
             .device
             .create_shader_module(wgpu::ShaderModuleDescriptor {
