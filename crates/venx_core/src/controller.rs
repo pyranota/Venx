8d6e5917508b674d74613f5c35d351a7836a940e
Pyra <gaast.dir@skiff.com>
2023-09-14 19:23:31 +0200

    Start to implement on/from gpu transfer

--- crates/venx_core/src/controller.rs
+++ crates/venx_core/src/controller.rs
@@ -1,18 +1,37 @@
-use crate::voxel_data::VXdata;
+use venx_compute::ComputeServer;
 
-/**
- *
- */
+use crate::{
+    shared::byte_casting::any_as_u8_slice,
+    voxel::{cpu, data::VXdata, gpu::voxel::VoxelGpu},
+};
+
 pub(crate) struct Controller {
     data: VXdata,
+    cs: ComputeServer,
 }
 
 #[cfg(feature = "gpu")]
 impl Controller {
+    pub(crate) fn new() -> Self {
+        todo!()
+    }
     pub(crate) async fn transfer_to_gpu(self) -> Self {
+        let mut gpu_data = VoxelGpu {
+            attribute: todo!(),
+            topology: todo!(),
+        };
+
+        if let (VXdata::Cpu(cpu_data), cs) = (self.data, self.cs) {
+            let (attribute, topology) = (cpu_data.attribute, cpu_data.topology);
+
+            let attr_buffer = cs.new_buffer(unsafe { any_as_u8_slice(&attribute) });
+        } else {
+            return self;
+        }
         todo!()
     }
     pub(crate) async fn transfer_to_cpu(self) -> Self {
+        if let VXdata::Gpu(gpu_data) = self.data {}
         todo!()
     }
     pub(crate) async fn toggle(self) -> Self {
