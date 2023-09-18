ec6f0f9eb9327beff7df7d862594395df784c8a2
Gaast <gaast.dir@skiff.com>
2023-09-18 17:48:02 +0200

    Add `new` method for cpu Voxel

--- crates/venx_core/src/voxel/cpu/voxel.rs
+++ crates/venx_core/src/voxel/cpu/voxel.rs
@@ -3,16 +3,41 @@ use crate::voxel::vx_trait::VoxelTrait;
 use super::{attribute::tetree::TeTree, topology::graph::Graph};
 
 pub struct Voxel {
-    pub attribute: TeTree,
+    pub(crate) attribute: TeTree,
     pub topology: Graph,
 }
 
-impl VoxelTrait for Voxel {
-    fn insert_segment(segment: crate::voxel::segment::Segment, position: glam::UVec3) {
-        todo!()
-    }
-
-    fn load_chunk(position: glam::UVec3, level: u8) -> crate::chunk::chunk::Chunk {
-        todo!()
+impl Voxel {
+    pub fn new(depth: u8) -> Self {
+        Voxel {
+            attribute: TeTree { nodes: vec![] },
+            topology: Graph::new(depth),
+        }
     }
 }
+
+// impl VoxelTrait for Voxel {
+//     fn insert_segment(segment: crate::voxel::segment::Segment, position: glam::UVec3) {
+//         todo!()
+//     }
+
+//     fn load_chunk(position: glam::UVec3, level: u8) -> crate::chunk::chunk::Chunk {
+//         todo!()
+//     }
+
+//     fn load_chunks(position: glam::UVec3, level: u8) -> crate::chunk::chunk::Chunk {
+//         todo!()
+//     }
+
+//     fn load_chunk_n_mesh() {
+//         todo!()
+//     }
+
+//     fn load_chunks_n_meshes() {
+//         todo!()
+//     }
+
+//     fn compute_mesh_from_chunk() {
+//         todo!()
+//     }
+// }
