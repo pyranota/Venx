494c3128556e76e5eae924f955f267d7e4aff72c
Gaast <gaast.dir@skiff.com>
2023-09-22 16:53:19 +0200

    `VoxelTrait` is now almost implemented by `Voxel`

--- crates/venx_core/src/voxel/cpu/mod.rs
+++ crates/venx_core/src/voxel/cpu/mod.rs
@@ -1,7 +1,7 @@
 pub mod attribute;
 mod insert;
 mod load;
-mod mesh;
+pub mod mesh;
 pub mod topology;
 pub mod traverse;
 pub mod utils;
--- crates/venx_core/src/voxel/cpu/voxel.rs
+++ crates/venx_core/src/voxel/cpu/voxel.rs
@@ -1,4 +1,10 @@
-use crate::voxel::vx_trait::VoxelTrait;
+use glam::uvec3;
+
+use crate::{
+    chunk::chunk::Chunk,
+    plat::Plat,
+    voxel::{segment::Segment, vx_trait::VoxelTrait},
+};
 
 use super::{attribute::tetree::TeTree, topology::graph::Graph};
 
@@ -23,27 +29,61 @@ impl Voxel {
 
 impl VoxelTrait for Voxel {
     fn insert_segment(&mut self, segment: crate::voxel::segment::Segment, position: glam::UVec3) {
-        
-        todo!()
+        let offset = segment.size() * position;
+        segment.iter(|pos, block| {
+            // Redo
+
+            if block != 0 {
+                self.topology.set(offset + pos, true);
+            }
+        });
     }
 
-    fn load_chunk(&self, position: glam::UVec3, level: u8) -> crate::chunk::chunk::Chunk {
-        todo!()
+    fn load_chunk(
+        &self,
+        position: glam::UVec3,
+        level: u8,
+    ) -> std::option::Option<crate::chunk::chunk::Chunk> {
+        self.load_chunk(position, level)
     }
 
     fn load_chunks(&self, position: glam::UVec3, level: u8) -> crate::chunk::chunk::Chunk {
         todo!()
     }
 
-    fn load_chunk_n_mesh() {
+    fn load_chunk_n_mesh(&self) {
         todo!()
     }
 
-    fn load_chunks_n_meshes() {
+    fn load_chunks_n_meshes(&self) {
         todo!()
     }
 
-    fn compute_mesh_from_chunk() {
-        todo!()
+    fn compute_mesh_from_chunk(&self, chunk: &Chunk) -> super::mesh::Mesh {
+        self.to_mesh_naive(chunk)
     }
+
+    fn get(&self, level: u8, position: glam::UVec3) -> Option<usize> {
+        self.topology.get(level, position)
+    }
+}
+
+#[test]
+fn test_insert_segment() {
+    let mut plat = Plat::new(5, 2, 4);
+    let mut segment = Segment::new(4);
+    segment.set(uvec3(15, 0, 11), 11);
+
+    plat.insert_segment(segment, uvec3(0, 0, 0));
+
+    let mut segment = Segment::new(4);
+    segment.set(uvec3(0, 5, 0), 15);
+
+    plat.insert_segment(segment, uvec3(0, 1, 0));
+
+    plat.get(0, uvec3(15, 0, 11)).unwrap();
+    plat.get(0, uvec3(0, 16 + 5, 0)).unwrap();
+    assert_eq!(plat.get(0, uvec3(15, 0, 11) + uvec3(0, 16, 0)), None);
+    assert_eq!(plat.get(0, uvec3(0, 0, 0) + uvec3(0, 0, 0)), None);
+    assert_eq!(plat.get(0, uvec3(19, 0, 11) + uvec3(16, 16, 0)), None);
 }
