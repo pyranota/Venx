d7cb2fc606cbdfc2974018b062af186287dcc1f4
Gaast <gaast.dir@skiff.com>
2023-09-22 16:58:29 +0200

    Partially implement neighbor get for chunk

--- crates/venx_core/src/chunk/mod.rs
+++ crates/venx_core/src/chunk/mod.rs
@@ -1,2 +1,3 @@
 pub mod chunk;
+pub mod neighbor;
 pub mod storage;
---
+++ crates/venx_core/src/chunk/neighbor.rs
@@ -0,0 +1,42 @@
+use glam::{IVec3, UVec3, Vec3};
+
+use crate::voxel::cpu::voxel::Voxel;
+
+use super::chunk::Chunk;
+
+impl Voxel {
+    pub fn get_neighbor(
+        &self,
+        chunk: &Chunk,
+        local_block_position: impl Into<IVec3>,
+        neighbor_direction: impl Into<IVec3>,
+    ) -> Option<bool> {
+        let chunk_size = chunk.size();
+        let dir: IVec3 = neighbor_direction.into();
+        let pos: IVec3 = local_block_position.into();
+        let sum = pos + dir;
+
+        if sum.min_element() < 0 {
+            return None;
+        } else if sum.max_element() >= chunk_size as i32 {
+            return None;
+        } else {
+            return chunk.get(sum.as_uvec3());
+        }
+    }
+    pub fn get_neighbor_untyped(
+        &self,
+        chunk: &Chunk,
+        block_position: impl Into<UVec3>,
+        neighbor_direction: impl Into<UVec3>,
+    ) -> Option<u32> {
+        todo!()
+    }
+}
+
+#[test]
+fn neighbor_test() {
+    let chunk = Chunk::new(UVec3::ZERO, 0, 4);
+
+    // chunk.get_neighbor((13, 1, 3), (7, 1, 1));
+}
