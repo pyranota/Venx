b76495cf89b77eb1756ae83c20688079a2724e59
Gaast <gaast.dir@skiff.com>
2023-09-22 16:55:04 +0200

    Many changes in `VoxelTrait`

--- crates/venx_core/src/voxel/vx_trait.rs
+++ crates/venx_core/src/voxel/vx_trait.rs
@@ -2,15 +2,19 @@ use glam::UVec3;
 
 use crate::chunk::chunk::Chunk;
 
-use super::segment::{Segment, SegmentStatic};
+use super::{
+    cpu::mesh::Mesh,
+    segment::{Segment, SegmentStatic},
+};
 
 pub trait VoxelTrait {
     fn insert_segment(&mut self, segment: Segment, position: UVec3);
-    fn load_chunk(&self, position: UVec3, level: u8) -> Chunk;
+    fn load_chunk(&self, position: UVec3, level: u8) -> Option<Chunk>;
     fn load_chunks(&self, position: UVec3, level: u8) -> Chunk;
-    fn load_chunk_n_mesh();
-    fn load_chunks_n_meshes();
-    fn compute_mesh_from_chunk();
+    fn load_chunk_n_mesh(&self);
+    fn load_chunks_n_meshes(&self);
+    fn compute_mesh_from_chunk(&self, chunk: &Chunk) -> Mesh;
+    fn get(&self, level: u8, position: UVec3) -> Option<usize>;
     /*
     fn compute_mesh_turbo();
     fn empty_segment();
