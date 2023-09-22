c4a479259f6ca277520c7c2d72ee7c32e7c9638a
Gaast <gaast.dir@skiff.com>
2023-09-22 17:00:13 +0200

    Add sunlight to test scene

--- crates/venx_core/bin/temp_bevy_integration/main/mod.rs
+++ crates/venx_core/bin/temp_bevy_integration/main/mod.rs
@@ -1,7 +1,9 @@
 //! A simple 3D scene with light shining over a cube sitting on a plane.
 pub mod plat;
 
-use bevy::prelude::*;
+use std::f32::consts::PI;
+
+use bevy::{pbr::CascadeShadowConfigBuilder, prelude::*};
 use bevy_panorbit_camera::PanOrbitCamera;
 
 pub struct Venx;
@@ -19,29 +21,60 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    // // plane
-    // commands.spawn(PbrBundle {
-    //     mesh: meshes.add(shape::Plane::from_size(5.0).into()),
-    //     material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
-    //     ..default()
-    // });
-    // // cube
-    // commands.spawn(PbrBundle {
-    //     mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-    //     material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-    //     transform: Transform::from_xyz(0.0, 0.5, 0.0),
-    //     ..default()
+    // plane
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(shape::Plane::from_size(5.0).into()),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..default()
+    });
+    // cube
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        transform: Transform::from_xyz(-10.0, 300.0, -10.0),
+        ..default()
+    });
+
+    // // ambient light
+    // commands.insert_resource(AmbientLight {
+    //     color: Color::ORANGE_RED,
+    //     brightness: 0.03,
     // });
     // light
-    commands.spawn(PointLightBundle {
-        point_light: PointLight {
-            intensity: 1500.0,
+    // commands.spawn(PointLightBundle {
+    //     point_light: PointLight {
+    //         intensity: 4000.0,
+    //         shadows_enabled: true,
+    //         range: 400.,
+    //         radius: 200.,
+    //         ..default()
+    //     },
+    //     transform: Transform::from_xyz(0.0, 15.0, 0.0),
+    //     ..default()
+    // });
+    // directional 'sun' light
+    commands.spawn(DirectionalLightBundle {
+        directional_light: DirectionalLight {
             shadows_enabled: true,
             ..default()
         },
-        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        transform: Transform {
+            translation: Vec3::new(-10.0, 300.0, -10.0),
+            rotation: Quat::from_rotation_x(-PI / 4.),
+            ..default()
+        },
+        // The default cascade config is designed to handle large scenes.
+        // As this example has a much smaller world, we can tighten the shadow
+        // bounds for better visual quality.
+        // cascade_shadow_config: CascadeShadowConfigBuilder {
+        //     first_cascade_far_bound: 4.0,
+        //     maximum_distance: 10.0,
+        //     ..default()
+        // }
+        // .into(),
         ..default()
     });
+
     // camera
     commands.spawn((
         Camera3dBundle {
