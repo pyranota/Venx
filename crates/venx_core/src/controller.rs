4b4421849feafcd0ef162f499ee2dd7073dac0fa
Gaast <gaast.dir@skiff.com>
2023-09-22 16:59:40 +0200

    Impl helper function for controller for getting vx

--- crates/venx_core/src/controller.rs
+++ crates/venx_core/src/controller.rs
@@ -2,19 +2,41 @@ use venx_compute::ComputeServer;
 
 use crate::{
     shared::byte_casting::any_as_u8_slice,
-    voxel::{cpu, data::VXdata, gpu::voxel::VoxelGpu},
+    voxel::{
+        cpu::{self, voxel::Voxel},
+        data::VXdata,
+        gpu::voxel::VoxelGpu,
+        vx_trait::VoxelTrait,
+    },
 };
 
 pub(crate) struct Controller {
     data: VXdata,
     cs: ComputeServer,
 }
+impl Controller {
+    pub(crate) fn new(depth: u8, chunk_level: u8, segment_level: u8) -> Self {
+        Self {
+            data: VXdata::Cpu(Voxel::new(depth, chunk_level, segment_level)),
+            cs: pollster::block_on(ComputeServer::new()),
+        }
+    }
+    pub(crate) fn get_voxel(&self) -> Box<&dyn VoxelTrait> {
+        Box::new(match &self.data {
+            VXdata::Cpu(vx) => vx,
+            VXdata::Gpu(vx) => vx,
+        })
+    }
+    pub(crate) fn get_voxel_mut(&mut self) -> Box<&mut dyn VoxelTrait> {
+        Box::new(match &mut self.data {
+            VXdata::Cpu(vx) => vx,
+            VXdata::Gpu(vx) => vx,
+        })
+    }
+}
 
 #[cfg(feature = "gpu")]
 impl Controller {
-    pub(crate) fn new() -> Self {
-        todo!()
-    }
     pub(crate) async fn transfer_to_gpu(self) -> Self {
         let mut gpu_data = VoxelGpu {
             attribute: todo!(),
