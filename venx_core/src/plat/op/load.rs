8c67c3de5a80cef02c35eb6121da14cc409034db
Ash <->
2024-02-05 19:09:34 +0100

    Fix load_chunk + write test

--- venx_core/src/plat/op/load.rs
+++ venx_core/src/plat/op/load.rs
@@ -8,13 +8,13 @@ use crate::{
 use super::LayerOpts;
 
 impl RawPlat {
-    pub fn load_chunk(&self, position: UVec3, level: u8) -> Chunk {
+    pub fn load_chunk(&self, position: UVec3, lod_level: u8) -> Chunk {
         // TODO change
         let chunk_level = 5;
-        let mut chunk = Chunk::new(position, level, chunk_level);
-        let chunk_lod_scaler = l2s(level);
+        let mut chunk = Chunk::new(position, lod_level, chunk_level);
+        let chunk_lod_scaler = l2s(lod_level);
 
-        let real_chunk_size = l2s(chunk.level());
+        // let real_chunk_size = l2s(chunk.level());
 
         self.traverse_region(
             position,
@@ -22,7 +22,7 @@ impl RawPlat {
             super::EntryOpts::All,
             LayerOpts::All,
             &mut |props| {
-                if props.level == level {
+                if props.level == lod_level {
                     chunk.set(props.position.unwrap() / chunk_lod_scaler, props.entry);
                     return false;
                 }
@@ -33,13 +33,26 @@ impl RawPlat {
     }
 }
 
-#[test]
-fn load_chunk() {
-    let mut plat = RawPlat::new(7, 5, 6);
+#[cfg(test)]
+mod tests {
+    use std::println;
 
-    plat[0].set(uvec3(0, 0, 0), 2);
+    use spirv_std::glam::uvec3;
 
-    let chunk = plat.load_chunk(UVec3::ZERO, 0);
+    use crate::plat::raw_plat::RawPlat;
 
-    assert!(chunk.get(uvec3(0, 0, 0)).is_some());
+    extern crate std;
+    #[test]
+    fn load_chunk() {
+        let mut plat = RawPlat::new(6, 5, 5);
+
+        plat[0].set(uvec3(15, 15, 15), 1);
+        plat[0].set(uvec3(0, 0, 0), 2);
+
+        let chunk = plat.load_chunk(uvec3(0, 0, 0), 0);
+
+        //println!("{:?}", chunk);
+
+        assert!(chunk.get(uvec3(0, 0, 0)).is_some());
+    }
 }
