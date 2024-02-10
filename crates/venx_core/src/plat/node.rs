449d744e45c4cd821006d43ae42081b56887349b
ggaast <gaast.dir@skiff.com>
2024-02-10 19:59:49 +0100

    Implement NodeAddr

--- crates/venx_core/src/plat/node.rs
+++ crates/venx_core/src/plat/node.rs
@@ -1,7 +1,9 @@
-use core::fmt::Debug;
+use core::{fmt::Debug, ops::Index};
 
 use bytemuck::{Pod, Zeroable};
 use spirv_std::glam::UVec3;
+
+use crate::utils::l2s;
 
 #[repr(C)]
 #[derive(Copy, Debug, Clone, Default, PartialEq, PartialOrd, Pod, Zeroable)]
@@ -43,5 +45,84 @@ impl Node {
             flag,
             children: Default::default(),
         }
+    }
+}
+// TODO: implement From<u64>
+pub struct NodeAddr(u64);
+
+impl NodeAddr {
+    pub fn from_position(mut position: UVec3, depth: usize) -> Self {
+        assert!(depth <= 21);
+        let mut addr = Self::new();
+        let mut level = depth;
+        let mut size = l2s(depth);
+        while level > 0 {
+            {
+                let child_index = Node::get_child_index(position, level - 1);
+                addr.set_idx(level, child_index);
+            }
+
+            {
+                size /= 2;
+                level -= 1;
+                position.x %= size;
+                position.y %= size;
+                position.z %= size;
+            }
+        }
+
+        addr
+    }
+    pub(crate) fn new() -> Self {
+        NodeAddr(0)
+    }
+    /// Get child idx on given level
+    pub fn get_idx(&self, level: usize) -> usize {
+        assert!(level <= 21);
+        ((&self.0 >> (level * 3)) & 0o7u64) as usize
+    }
+    /// Set child idx on given level
+    /// Idx is 0-7
+    pub fn set_idx(&mut self, level: usize, idx: usize) {
+        assert!(level <= 21);
+        assert!(idx < 8);
+        self.0 |= (idx as u64) << level * 3;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use spirv_std::glam::uvec3;
+
+    use super::NodeAddr;
+
+    #[test]
+    fn test_node_addr() {
+        let mut addr = NodeAddr::new();
+        addr.set_idx(5, 0);
+        addr.set_idx(4, 5);
+        addr.set_idx(3, 7);
+        addr.set_idx(2, 1);
+        addr.set_idx(1, 5);
+        addr.set_idx(0, 6);
+
+        assert_eq!(addr.get_idx(5), 0);
+        assert_eq!(addr.get_idx(4), 5);
+        assert_eq!(addr.get_idx(3), 7);
+        assert_eq!(addr.get_idx(2), 1);
+        assert_eq!(addr.get_idx(1), 5);
+        assert_eq!(addr.get_idx(0), 6);
+    }
+
+    #[test]
+    fn test_node_addr_from_position() {
+        let addr = NodeAddr::from_position(uvec3(7, 20, 5), 5);
+
+        assert_eq!(addr.get_idx(5), 2);
+        assert_eq!(addr.get_idx(4), 0);
+        assert_eq!(addr.get_idx(3), 7);
+        assert_eq!(addr.get_idx(2), 1);
+        assert_eq!(addr.get_idx(1), 5);
+        assert_eq!(addr.get_idx(0), 0);
     }
 }
