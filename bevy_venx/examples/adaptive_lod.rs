e902b0ca75d5ec297c082d470c549b9ce87be321
ggaast <gaast.dir@skiff.com>
2024-02-17 14:59:21 +0100

    Change values in example

--- bevy_venx/examples/adaptive_lod.rs
+++ bevy_venx/examples/adaptive_lod.rs
@@ -31,7 +31,7 @@ fn setup(
         plat.save("ALOD").unwrap();
         plat
     });
-    for mesh in plat.static_mesh(8..12, 3..7, 8..12, Some(0)) {
+    for mesh in plat.static_mesh(0..16, 3..7, 0..16, Some(1)) {
         let mut bevy_mesh = Mesh::new(PrimitiveTopology::TriangleList);
 
         bevy_mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, mesh.0.clone());
