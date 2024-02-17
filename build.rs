d5c1ae6bfa43a3cd1f7cfc4c3a230d826747b534
ggaast <gaast.dir@skiff.com>
2024-02-17 14:58:40 +0100

    Update target

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 use spirv_builder::{MetadataPrintout, SpirvBuilder};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    SpirvBuilder::new("crates/venx_shaders", "spirv-unknown-vulkan1.1")
+    SpirvBuilder::new("crates/venx_shaders", "spirv-unknown-vulkan1.2")
         .print_metadata(MetadataPrintout::Full)
         .build()?;
     Ok(())
