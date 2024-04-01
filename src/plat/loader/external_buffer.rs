33b7d20831b20547c9ae57e422cd97190d5a0c31
gaast <ggaast@proton.me>
2024-04-01 21:59:25 +0200

    Change `ExternalBuffer::set` arguments

--- src/plat/loader/external_buffer.rs
+++ src/plat/loader/external_buffer.rs
@@ -1,14 +1,21 @@
-const RESOLUTION: u64 = 100;
+use log::warn;
+
+/// Defines how many bytes can transfered within single call
+///
+/// Or bus size
+pub const EXTERNAL_BUFFER_RESOLUTION: u64 = 100;
 
 pub trait ExternalBuffer {
+    /// Set Buffer's data
+    ///
     /// Should be able to handle ranges atleast [RESOLUTION]
-    fn set(self: &Self, bounds: (u64, u64), data: Vec<u8>);
+    fn set(self: &Self, offset: u32, data: &[u8]);
 }
 
 pub struct FakeBuffer;
 
 impl ExternalBuffer for FakeBuffer {
-    fn set(self: &Self, _bounds: (u64, u64), _data: Vec<u8>) {
-        // Yeah :)
+    fn set(self: &Self, _offset: u32, _data: &[u8]) {
+        warn!("Using FakeBuffer")
     }
 }
